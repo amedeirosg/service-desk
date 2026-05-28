6ef25cf985154c440357a4828d3e5dc408347fdc
Medeiros <medeiirosdev@gmail.com>
2026-05-27 21:55:43 -0300

    feat: create User entity

---
+++ src/main/java/dev/medeiros/service_desk/entity/User.java
@@ -0,0 +1,38 @@
+package dev.medeiros.service_desk.entity;
+
+import dev.medeiros.service_desk.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+}
