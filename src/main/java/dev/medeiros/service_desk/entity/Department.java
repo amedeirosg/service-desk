ababa5d5b7d0cf689dd943c8aa7b6b7cc9753fa9
Medeiros <medeiirosdev@gmail.com>
2026-05-27 21:54:51 -0300

    feat: create department entity

---
+++ src/main/java/dev/medeiros/service_desk/entity/Department.java
@@ -0,0 +1,25 @@
+package dev.medeiros.service_desk.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long id;
+
+    private String name;
+
+
+}
