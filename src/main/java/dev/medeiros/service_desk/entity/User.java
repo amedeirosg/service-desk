1ca4f0d93f62ef635a11cd4a76f24db4bdd09140
Medeiros <medeiirosdev@gmail.com>
2026-05-28 14:02:36 -0300

    refactor: remove lombok all args constructor

--- src/main/java/dev/medeiros/service_desk/entity/User.java
+++ src/main/java/dev/medeiros/service_desk/entity/User.java
@@ -2,17 +2,18 @@ package dev.medeiros.service_desk.entity;
 
 import dev.medeiros.service_desk.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.jspecify.annotations.Nullable;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -34,5 +35,11 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-
+    public User(String name, String email, String password, Department department, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.department = department;
+        this.role = role;
+    }
 }
