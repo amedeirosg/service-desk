0d6244b9a67f6705e24b552516e172812ee3b304
Medeiros <medeiirosdev@gmail.com>
2026-05-28 14:03:30 -0300

    feat: create user repository

---
+++ src/main/java/dev/medeiros/service_desk/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.medeiros.service_desk.repository;
+
+import dev.medeiros.service_desk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+}
